d5921506186ac8fa836ff72e828e0e485ba247da
Minh <18521092@gm.uit.edu.vn>
2021-01-24 21:35:42 +0700

    done slider

--- Frontend/bet-store-mobile/define.ts
+++ Frontend/bet-store-mobile/define.ts
@@ -1,10 +1,3 @@
-import Constants from "expo-constants";
-import { Platform } from "react-native";
-
-const HOST_LOCAL_EMU = "http://10.0.3.2";
-const HOST_LOCAL = "http://localhost";
-const Host_WIFI = "http://192.168.1.135";
-
 export const CDNAPI = "https://betstore.tk/cdn"
 export const GolangAPI = "https://betstore.tk/api/go"
 export const JavaAPI = `https://betstore.tk/api/java`;
