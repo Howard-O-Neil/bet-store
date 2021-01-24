8599a57ffdccaee19988cd459c343d79f2a2e470
Minh <18521092@gm.uit.edu.vn>
2021-01-25 01:28:46 +0700

    fix wallet

--- Frontend/bet-store-mobile/src/actions/walletAction.tsx
+++ Frontend/bet-store-mobile/src/actions/walletAction.tsx
@@ -1,5 +1,6 @@
 import Axios from 'axios';
 import { GolangAPI } from '../../define';
+import { GetItemInStorage } from '../components/AsyncStorageUtls';
 import {
   GET_INFO_WALLET,
   GET_INFO_WALLET_FAIL,
@@ -17,6 +18,9 @@ export const InfoWallet = () => async (
   dispatch: React.Dispatch<ActionType<WalletInfoType>>,
 ) => {
   dispatch(GetInfoWallet());
+
+  Axios.defaults.headers.common.Authentication =
+    'Bearer ' + await GetItemInStorage('token'); // for all requests
   let response = await Axios.get<ResponseAPI<WalletInfoType>>(`${GolangAPI}/wallet/`);
   if (response.status === 200) {
     if (response.data.status === 200) {
